4461a54e327491dcb9267d1c90bb755176e308ec
John Hobbs <john@velvetcache.org>
2011-08-18 13:52:39 -0500

    Undo accidental merge.

--- api.php
+++ api.php
@@ -13,39 +13,35 @@
 	ORM::configure( 'username', $config['database']['username'] );
 	ORM::configure( 'password', $config['database']['password'] );
 
-	/////////////////////////////////////////////////////////////////////
-	// Use create_function for PHP < 5.3
-	/////////////////////////////////////////////////////////////////////
-
 	$s = new Skunk();
 
 	$s->hook(
 		'send_head', 
-		create_function( '&$s', '
+		function ( &$s ) {
 			$s->header( "X-Made-With", "Joy" );
-		' )
+		}
 	);
 
 	$s->hook(
 		'before',
-		create_function ( '&$s', '
+		function ( &$s ) {
 			$s->header( "Content-Type", "application/json" ); 
 			$s->body = array();
-		' )
+		}
 	);
 
 	$s->hook(
 		'after',
-		create_function ( '&$s', '
+		function ( &$s ) {
 			if( is_array( $s->body ) ) { 
 				$s->body = json_encode( $s->body );
 			}
-		' )
+		}
 	);
 
 	$s->get(
 		'/launder/bulk(/)',
-		create_function ( '&$s',  '
+		function ( &$s ) {
 			if( ! isset( $_REQUEST["number"] ) || 0 == count( $_REQUEST["number"] ) ) {
 				$s->body["error"] = true;
 				$s->body["message"] = "No Numbers Given";
@@ -55,14 +51,14 @@
 			foreach( $_REQUEST["number"] as $number ) {
 				$s->body[] = ProcessNumber( $number );
 			}
-		' )
+		}
 	);
 
 	$s->get(
 		'/launder/<number>(/)',
-		create_function ( '&$s,$number', '
+		function ( &$s, $number ) {
 			$s->body = ProcessNumber( $number );
-		' )
+		}
 	);
 
 	$s->run( $_SERVER['PATH_INFO'] );
